018de7a44aed636e5132d1593ac0bcaadf7a63e9
Dmitry I. Protsenko <dprotsenko.repos@gmail.com>
2018-02-01 16:56:28 +0200

    @EqualsAndHashCode annotation

--- src/main/java/protsenko/Calc.java
+++ src/main/java/protsenko/Calc.java
@@ -1,7 +1,8 @@
 package protsenko;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode
 public final class Calc {
 
     private final int arg;
@@ -24,18 +25,5 @@ public final class Calc {
 
     public Calc square() {
         return new Calc(this.arg * this.arg);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Calc calc = (Calc) o;
-        return arg == calc.arg;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(arg);
     }
 }
